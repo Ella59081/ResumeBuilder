d25a2907ec0a8825c835a40214b4749898c7bc55
Ella59081 <elea123nor@gmail.com>
2025-05-22 16:00:42 +0100

    download

--- src/Pages/overview/overview.jsx
+++ src/Pages/overview/overview.jsx
@@ -1,17 +1,182 @@
 import { useContext, useState } from "react";
 import { FormContext } from "../../contexts/FormContext";
-import '../review/review.css'
+
+import './overview.css'
 
 function Overview(){
 
 
-    
+    const {formData} = useContext(FormContext);
 
     return(
         <>
          <div className="viewResume">
-            <h1>A basic review of your template</h1>
+            <h1>Overview</h1>
+            <div className="photo over">
+                                    <div className="temp-des">
+                                        <div className="side-in"></div>
+                                        <div className="main-in">
+                                            <div className="header-in">
+                                                <h6>
+                                                    <span>{formData.personalDetails.f_name ? formData.personalDetails.f_name.toUpperCase() + "  " : 'JEREMY' + "  "}</span>
+                                                    <span>{formData.personalDetails.l_name ? formData.personalDetails.l_name.toUpperCase() : 'FISHER'}</span>
+                                                </h6>
+                                                <p>{formData.experience.title1 ? formData.experience.title1.toUpperCase() : 'KNOWLEDGEABLE SOCIAL MEDIA MANAGER'}</p>
+                                            </div>
+                                            <div className="summary">
+                                                <div className="left">
+                                                    <div className="prof">
+                                                        <h6>PROFESSIONAL SUMMARY</h6>
+                                                        <p className="top">
+                                                            {
+                                                               formData.summary.professionalSummary ? formData.summary.professionalSummary :
+                                                               "Creative social media manager with proven record of success in building brand awareness, increasing followers and patnering with top influencers throughout the country."
+                                                            }
+                                                        </p>
+                                                    </div>
+                                                    <div className="work">
+                                                        <h6>WORK HISTORY</h6>
+                                                        <h6 className='top'>{formData.experience.title1 ? formData.experience.title1 : 'Social Media Manager'}</h6>
+                                                        <p>
+                                                            <span>{formData.experience.company ? formData.experience.company + ' ' : 'infasec' + ' '}</span>
+                                                            <span>
+                                                                {formData.experience.start_date ? formData.experience.title1 
+                                                                + ' ' + 'since' + ' ' + formData.experience.start_date.slice(0, 4) :
+                                                                'social media manager since march 2021'}
+                                                            </span>
+                                                        </p>
+                                                        <p className="top">
+                                                          {formData.experience.role1 ? '.' + ' ' + formData.experience.role1 : '. Develop marketing content'}
+                                                        </p>
+                                                        <p>{formData.experience.role2 ? '.' + ' ' + formData.experience.role2 : '. Analyze reported social media'}</p>
+                                                        <p>{formData.experience.role3 ? '.' + ' ' + formData.experience.role3 : '. Curate and segment content to increase engagement'}</p>
+                                                    </div>
+                                                    <div className="title2">
+                                                        <h6>
+                                                            {formData.experience2.job2 ? formData.experience2.job2 : 'Digital Marketing Coordinator'}
+                                                        </h6>
+                                                        
+                                                        <p>
+                                                            <span>{formData.experience2.company ? formData.experience2.company + ' ' : 'infasec' + ' '}</span>
+                                                            <span>
+                                                                {formData.experience2.end_date ? formData.experience2.job2
+                                                                + ' ' + 'from' + ' ' + formData.experience2.start_date.slice(0, 4) + 
+                                                                ' ' + 'to' + ' ' + formData.experience2.end_date.slice(0, 4) :
+                                                                'social media manager since march 2021'}
+                                                            </span>
+                                                        </p>
+                                                        <p className="top">
+                                                          {formData.experience2.role1 ? '.' + ' ' + formData.experience2.role1 : '. Develop marketing content'}
+                                                        </p>
+                                                        <p>
+                                                            {formData.experience2.role2 ? '.' + ' ' + formData.experience2.role2 : '. Analyze reported social media'}
+                                                        </p>
+                                                        <p>
+                                                            {formData.experience2.role3 ? '.' + ' ' + formData.experience2.role3 : '. Curate and segment content to increase engagement'}
+                                                        </p>
+                                                    </div>
+                                                    <div className="title2">
+                                                        
+                                                        <h6>
+                                                            {formData.experience3.job3 ? formData.experience3.job3 : 'Social Media Manager'}
+                                                        </h6>
+                                                        <p>
+                                                            <span>{formData.experience3.company ? formData.experience3.company + ' ' : 'infasec' + ' '}</span>
+                                                            <span>
+                                                                {formData.experience3.end_date ? formData.experience3.job3
+                                                                + ' ' + 'from' + ' ' + formData.experience3.start_date.slice(0, 4) + 
+                                                                ' ' + 'to' + ' ' + formData.experience3.end_date.slice(0, 4) :
+                                                                'social media manager since march 2021'}
+                                                            </span>
+                                                        </p>
+                                                        <p className="top">
+                                                          {formData.experience3.role1 ? '.' + ' ' + formData.experience3.role1 : '. Develop marketing content'}
+                                                        </p>
+                                                        <p>
+                                                            {formData.experience3.role2 ? '.' + ' ' + formData.experience3.role2 : '. Analyze reported social media'}
+                                                        </p>
+                                                        <p>
+                                                            {formData.experience3.role3 ? '.' + ' ' + formData.experience3.role3 : '. Curate and segment content to increase engagement'}
+                                                        </p>
+                                                    </div>
+                                                    
+                                                </div>
+                                                <div className="right">
+                                                    <div className="contact">
+                                                        <h6>CONTACT</h6>
+                                                        <p className='top'>
+                                                            <img src="/src/assets/images/phone-solid.svg" alt="" />
+                                                            {formData.personalDetails.phone ? formData.personalDetails.phone : '080 567 543 12'}
+                                                        </p>
+                                                        <p>
+                                                            <img src="/src/assets/images/envelope-solid.svg" alt="" /> 
+                                                            {formData.personalDetails.email ? formData.personalDetails.email : 'example@gmail.com'}
+                                                        </p>
+                                                        <p>
+                                                            <img src="/src/assets/images/location-dot-solid.svg" alt="" />
+                                                            {formData.personalDetails.address ? formData.personalDetails.address : 'maddison off close'}
+                                                        </p>
+                                                    </div>
+                                                    <div className="skills">
+                                                        <h6>SKILLS</h6>
+                                                        {
+                                                            formData.skills.skill1 ?
+                                                            Object.keys(formData.skills).map((key) =>(
+                                                                <p key={key}>. {formData.skills[key]}</p>
+                                                            ))
             
+                                                            :
+                                                            <div>
+                                                            <p>. Scheduling </p>
+                                                            <p>. Scheduling </p>
+                                                            <p>. Budgeting </p>
+                                                            <p>. Planning </p>
+                                                            </div>
+                                                            // '. Scheduling .Scheduling . love'
+                                                            
+                                                        }
+                        
+                                                    </div>
+                                                    <div className="education">
+                                                        <h6>EDUCATION</h6>
+                                                        <p className="top">
+                                                            {
+                                                                formData.education.degree ? formData.education.degree : 'Bachelor of arts'
+                                                            }
+                                                        </p>
+                                                        <p>
+                                                            {
+                                                                formData.education.field ? formData.education.field : 'Communications and marketing'
+                                                            }
+                                                        </p>
+                                                        <p>
+                                                            {
+                                                                formData.education.school ? formData.education.school : 'university of wiscondion'
+                                                            }
+                                                            <span>{
+                                                                formData.education.location ? ',' + ' ' + formData.education.location : ', New jersey'
+                                                            }</span>
+                                                        </p>
+                                                        <p>
+                                                            Graduated : 
+                                                            {
+                                                                formData.education.grad_date ? ' ' + formData.education.grad_date : ' 2003'
+                                                            }
+                                                        </p>
+                                                    </div>
+                                                    <div className="certifications">
+                                                        <h6>CERTIFICATION</h6>
+                                                        <p className="top">
+                                                            {
+                                                                formData.education.certification ? formData.education.certification : 'Data analytics'
+                                                            }
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
          </div>
         </>
     )
--- src/components/ResumePDF.jsx
+++ src/components/ResumePDF.jsx
@@ -1,2 +1,60 @@
 import React from "react";
-import { Page,Text,View, Document,StyleSheet } from "@react-pdf/renderer";
+import {
+    Page,
+    Text,
+    View, 
+    Document,
+    StyleSheet 
+} from "@react-pdf/renderer";
+
+const styles = StyleSheet.create({
+    page: {
+        flexDirection: 'row',
+        fontSize: 12,
+        fontFamily: 'Verdana'
+    },
+    sideBar: {
+        width: 80,
+        backgroundColor: 'blue',
+        height: '100%'
+    },
+    heading: {
+        textAlign: 'center',
+        paddingTop: 30
+    },
+    columnLeft: {
+        width: '60%',
+        padding: 30,
+        paddingRight: 0
+    },
+    columnRight: {
+        width: '40%',
+        padding: 30,
+    },
+    section: {
+        marginBottom: 30,
+    },
+    sectionTitle: {
+        fontSize: 14,
+        fontWeight: 'bold'
+    }
+
+    
+})
+
+
+const ResumePDF = ({FormData}) =>{
+
+}
+
+return(
+    <Document>
+        <Page size={'A4'} style={styles.page}>
+            {/* heading */}
+            <Text style={styles.heading}>
+                Your Name
+            </Text>
+            
+        </Page>
+    </Document>
+)
