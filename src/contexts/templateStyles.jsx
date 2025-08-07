71a8b3988e14a2c1871de4c6c874a8dfe5a53aa1
Ella59081 <elea123nor@gmail.com>
2025-08-07 18:49:08 +0100

    images

--- src/contexts/templateStyles.jsx
+++ src/contexts/templateStyles.jsx
@@ -1,32 +1,41 @@
+import temp1 from '../assets/images/template(1).svg'
+import temp2 from '../assets/images/template(2).svg'
+import temp3 from '../assets/images/resume-Template (1).avif'
+import temp4 from '../assets/images/template(3).svg'
+import temp5 from '../assets/images/template(4).svg'
+import temp6 from '../assets/images/template(1).svg'
+
+
+
+
 
 export const styles = {
     template1 : {
         sideColor: 'rgba(95, 195, 254, 1)',
         conColor: 'rgb(138, 212, 255)',
-        image: '/src/assets/images/template(1).svg'
+        image: temp1
     },
     template2 : {
         sideColor: 'whitesmoke',
         conColor: 'rgba(253, 206, 206, 1)',
-        image: '/src/assets/images/template(2).svg'
+        image: temp2
     },
     template3 : {
         sideColor: 'rgba(53, 74, 169, 1)',
         conColor: 'rgba(174, 185, 238, 1)',
-        image: '/src/assets/images/resume-Template (1).avif'
+        image: temp3
         
     },
     template4 : {
         sideColor: 'rgba(36, 113, 120, 1)',
         conColor: 'rgba(181, 217, 247, 1)',
-        image: '/src/assets/images/template(3).svg'
+        image: temp4
         
     },
     template5 : {
         sideColor: 'rgba(214, 70, 70, 1)',
         conColor: 'rgba(248, 166, 166, 1)',
-        image: '/src/assets/images/template(4).svg'
-        
+        image: temp5  
     },
     template6 : {
         sideColor: 'rgba(53, 74, 169, 1)',
